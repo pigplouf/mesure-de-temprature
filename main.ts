d924aba6e3ffb11efc03d77cf96d7f13966b1795
pigplouf <103496137+pigplouf@users.noreply.github.com>
2022-04-19 11:00:57 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
 basic.forever(function () {
     if (input.buttonIsPressed(Button.A)) {
-    	
+        basic.clearScreen()
+        basic.showString("T=")
+        basic.showString("DEGRES")
+        basic.showNumber(input.temperature())
     } else {
         basic.showLeds(`
             . . # . .
